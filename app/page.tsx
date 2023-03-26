1494c8a4006c289d45591e38d0b16d79c532914b
Lance Liang <liangxianyi2011@gmail.com>
2023-03-26 19:29:12 -0400

    Add croll back

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,15 @@ export default function Home() {
       <Portfolio />
       <br />
       <Gallery />
+      <br />
+      <a
+        href='#'
+        type='button'
+        className=' text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+      >
+        Top
+        <span className='sr-only'>Icon description</span>
+      </a>
     </>
   );
 }
