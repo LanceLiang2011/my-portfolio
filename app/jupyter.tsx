0d1c533d81cca83ff7332df6f5ebb545be305517
Lance Liang <liangxianyi2011@gmail.com>
2023-06-01 23:41:54 -0400

    Update a DS project

--- app/jupyter.tsx
+++ app/jupyter.tsx
@@ -17,6 +17,27 @@ export default function Jupyter() {
         </div>
         <div className="mx-auto my-6 max-w-7xl p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-6">
           <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#" target="_blank">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Transfer Learning to Classify Different Dog Breeds
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              This project is a milestone project of Complete Machine Learning &
+              Data Science Bootcamp inspired by the Kaggle project Dog Breed
+              Identification. The learning purpose is to building image
+              classification models with transfer learning and TensorFlow Hub.
+            </p>
+            <a
+              href="#"
+              target="_blank"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+            </a>
+          </div>
+
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a
               href="https://github.com/LanceLiang2011/Blue_Book_for_Bulldozers/blob/main/Blue%20Book%20for%20Bulldozers.ipynb"
               target="_blank"
