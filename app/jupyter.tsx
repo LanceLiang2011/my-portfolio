dfce35594c770e733c9c02c1ca770c49a195f852
Lance Liang <liangxianyi2011@gmail.com>
2023-06-11 03:49:17 -0400

    uploag notebook

--- app/jupyter.tsx
+++ app/jupyter.tsx
@@ -41,7 +41,10 @@ export default function Jupyter() {
           </div>
 
           <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#" target="_blank">
+            <a
+              href="https://github.com/LanceLiang2011/tree-models/blob/main/00-Sup-Learning-Capstone-Tree-Methods%20.ipynb"
+              target="_blank"
+            >
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 Ensemble Models for Churn Classification
               </h5>
@@ -54,7 +57,7 @@ export default function Jupyter() {
               perfomance.
             </p>
             <a
-              href="#"
+              href="https://github.com/LanceLiang2011/tree-models/blob/main/00-Sup-Learning-Capstone-Tree-Methods%20.ipynb"
               target="_blank"
               className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
