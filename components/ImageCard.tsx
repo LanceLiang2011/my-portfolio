cc7fe5f5c9b952229489cc5f43713a25b467128e
Lance Liang <liangxianyi2011@gmail.com>
2023-06-26 23:15:58 -0400

    change button color

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 interface Props {
   src: any;
@@ -16,31 +16,33 @@ export const ImageCard = ({
   alt,
   title,
   paragraph,
-  button = 'Visit',
-  href = '',
+  button = "Visit",
+  href = "",
   skills,
   shadow = true,
 }: Props) => {
   if (shadow)
     return (
-      <div className='card bg-base-100 shadow-xl image-full'>
+      <div className="card bg-base-100 shadow-xl image-full">
         <figure>
           <Image src={src} alt={alt} />
         </figure>
-        <div className='card-body'>
-          <h4 className='card-title text-gray-50'>{title}</h4>
-          <div className='flex gap-2'>
+        <div className="card-body">
+          <h4 className="card-title text-gray-50">{title}</h4>
+          <div className="flex gap-2">
             {skills &&
               skills.map((skill) => (
-                <div key={skill} className={`badge badge-primary`}>
+                <div key={skill} className={`badge badge-blue`}>
                   {skill}
                 </div>
               ))}
           </div>
-          <h5 className='text-gray-50'>{paragraph}</h5>
-          <div className='card-actions justify-end'>
-            <a href={href} target='_blank'>
-              <button className='btn btn-primary'>{button}</button>
+          <h5 className="text-gray-50">{paragraph}</h5>
+          <div className="card-actions justify-end">
+            <a href={href} target="_blank">
+              <button className="btn btn-primary text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                {button}
+              </button>
             </a>
           </div>
         </div>
@@ -48,13 +50,13 @@ export const ImageCard = ({
     );
 
   return (
-    <div className='card bg-base-100 shadow-xl'>
-      <figure className='px-10 pt-10'>
-        <Image src={src} alt={alt} className='rounded-xl' />
+    <div className="card bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <Image src={src} alt={alt} className="rounded-xl" />
       </figure>
-      <div className='card-body items-center text-center'>
-        <h2 className='card-title'>{title}</h2>
-        <div className='flex gap-2'>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{title}</h2>
+        <div className="flex gap-2">
           {skills &&
             skills.map((skill) => (
               <div key={skill} className={`badge badge-outline`}>
@@ -63,8 +65,8 @@ export const ImageCard = ({
             ))}
         </div>
         <p>{paragraph}</p>
-        <div className='card-actions'>
-          <button className='btn btn-primary'>{button}</button>
+        <div className="card-actions">
+          <button className="btn btn-primary">{button}</button>
         </div>
       </div>
     </div>
